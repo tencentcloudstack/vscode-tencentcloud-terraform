702e9cb66eed9e0a7176eeabd8f817874c9031f0
nickyinluo <nickyinluo@tencent.com>
2023-10-25 19:36:57 +0800

    1.scrape tool. 2.adjust package.json for npm compile.

--- tool/scrape/README.md
+++ tool/scrape/README.md
@@ -6,5 +6,3 @@ Scraping tool used to try to extract data from the terraform docs for the Visual
 
 This will output any notices to stderr and output the json that needs to go in vscode-tencentcloud-terraform/xxx.json to stdout. 
 - To generate resource defination, run: `docker run -it tiat/vscode-terraform-scrape > ../../config/tips/tiat-resources.json`.
-- To generate resource url link, run: `docker run -it tiat/vscode-terraform-scrape > ../../config/tips/tiat-urls.json`.
-
--- tool/scrape/build.sh
+++ tool/scrape/build.sh
@@ -11,7 +11,9 @@ else
     if [ ! -n "${2}" ]; then
         echo "docker build with [specified image]...[${1}][${2}][${workdir}]"
         docker build -f ${workdir}/Dockerfile ${workdir}/ -t ${1}
-        # docker build -f /Users/luoyin/Code/terraform-scrape/Dockerfile /Users/luoyin/Code/terraform-autocomplete -t tiat-terraform-scrape:v1
+
+        echo "docker run [${1}] to generate tiat-resources.json."
+        docker run -it ${1} >../../config/tips/tiat-resources.json
     else
         echo "docker build with [build-arg]...[${1}][${2}][${workdir}]"
         docker build --build-arg base=${2} -f ${workdir}/Dockerfile ${workdir}/ -t ${1}
