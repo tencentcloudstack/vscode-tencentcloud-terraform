27a451e020c61492dd10767245eedfee8fe6a10c
nickyinluo <nickyinluo@tencent.com>
2023-11-21 20:28:19 +0800

    support to scrape example from doc

--- tool/scrape/index.ts
+++ tool/scrape/index.ts
@@ -25,29 +25,34 @@ interface UrlPathNodes {
 
 interface Resource {
     name: string;
-    args: Variable[];
-    attrs: Variable[];
+    args: variable[];
+    attrs: variable[];
     url: string;
 }
 
-interface Variable {
+interface Snippet {
+    name: string;
+    example: string;
+}
+
+interface variable {
     name: string;
     description: string;
 }
 
-const resourcesPath = "terraform-provider-tencentcloud/website/docs/r/";
-const dataSourcePath = "terraform-provider-tencentcloud/website/docs/d/";
-const indexPath = "terraform-provider-tencentcloud/website/tencentcloud.erb";
+const resourcesPath = "/Users/luoyin/Code/terraform-provider-tencentcloud/website/docs/r/";
+const dataSourcePath = "/Users/luoyin/Code/terraform-provider-tencentcloud/website/docs/d/";
+const indexPath = "/Users/luoyin/Code/terraform-provider-tencentcloud/website/tencentcloud.erb";
 // ================================== FUNCTIONS ==================================
 
 function getParsed(filename: string): Promise<cheerio.Root> {
     return new Promise((resolve, reject) => {
-        var file = fs.readFileSync(resourcesPath + filename) + "";
+        let file = fs.readFileSync(resourcesPath + filename) + "";
         marked(file, (err, result) => {
             if (err) {
                 return reject(err);
             }
-            var $ = load(result);
+            let $ = load(result);
             resolve($);
         });
     });
@@ -60,7 +65,7 @@ function getAllParsed(files: string[]): Promise<cheerio.Root>[] {
 }
 
 function getNumWithArgumentReference($s: cheerio.Root[]): number {
-    var result = _.map($s, $ => {
+    let result = _.map($s, $ => {
         return $("h2").filter((z, el) => {
             return $(el).text() === "Argument Reference";
         }).length;
@@ -69,7 +74,7 @@ function getNumWithArgumentReference($s: cheerio.Root[]): number {
 }
 
 function getNumWithAttributesReference($s: cheerio.Root[]): number {
-    var result = _.map($s, $ => {
+    let result = _.map($s, $ => {
         return $("h2").filter((z, el) => {
             return $(el).text() === "Attributes Reference";
         }).length;
@@ -83,19 +88,19 @@ function getNumWithAttributesReference($s: cheerio.Root[]): number {
  * @param {*} $ - The full page as a cheerio object
  */
 function extractArgumentsContent($: cheerio.Root): ArgumentNodes {
-    var argsH2 = $("h2").filter((z, el) => {
+    let argsH2 = $("h2").filter((z, el) => {
         return $(el).text() === "Argument Reference";
     });
     if (argsH2.length !== 1) {
         throw "Didn't find correct number of h2 > Arguments Reference";
     }
-    var nodes = [];
-    var currentNode: any = argsH2[0];
+    let nodes = [];
+    let currentNode: any = argsH2[0];
     while (true) {
         if (!(currentNode.type === "text" && currentNode["data"] === "\n")) {
             nodes.push(currentNode);
         }
-        var nextSibling = _.get(currentNode, "nextSibling");
+        let nextSibling = _.get(currentNode, "nextSibling");
         if (!nextSibling || _.get(nextSibling, "name") === "h2") {
             break;
         }
@@ -105,7 +110,7 @@ function extractArgumentsContent($: cheerio.Root): ArgumentNodes {
 }
 
 function extractAttributesContent($: cheerio.Root): AttributeNodes {
-    var argsH2 = $("h2").filter((z, el) => {
+    let argsH2 = $("h2").filter((z, el) => {
         return $(el).text() === "Attribute Reference" || $(el).text() === "Attributes Reference";
     });
     if (argsH2.length !== 1) {
@@ -113,13 +118,13 @@ function extractAttributesContent($: cheerio.Root): AttributeNodes {
         return { attributeNodes: [] };
         // throw `Didn't find correct number of h2 > Attributes Reference on ${extractResourceName($)}`;
     }
-    var nodes = [];
-    var currentNode: any = argsH2[0];
+    let nodes = [];
+    let currentNode: any = argsH2[0];
     while (true) {
         if (!(currentNode.type === "text" && currentNode["data"] === "\n")) {
             nodes.push(currentNode);
         }
-        var nextSibling = _.get(currentNode, "nextSibling");
+        let nextSibling = _.get(currentNode, "nextSibling");
         if (!nextSibling || _.get(nextSibling, "name") === "h2") {
             break;
         }
@@ -128,33 +133,62 @@ function extractAttributesContent($: cheerio.Root): AttributeNodes {
     return { attributeNodes: nodes };
 }
 
-// function extractExampleContents($: cheerio.Root): SnippetNodes {
-//     var argsH2 = $("h2").filter((z, el) => {
-//         return $(el).text() == "Example Usage";
-//     });
-//     if (argsH2.length != 1) {
-//         console.error(`Didn't find any example on ${extractResourceName($)}`);
-//         throw "Didn't find correct number of h2 > Example Usage";
-//     }
-// }
+function extractExampleContent($: cheerio.Root): string {
+    let expH2 = $("h2").filter((z, el) => {
+        return $(el).text() === "Example Usage";
+    });
+    if (expH2.length !== 1) {
+        console.error(`Didn't find any example on ${extractResourceName($)}`);
+        return "";
+        // throw "Didn't find correct number of h2 > Example Usage";
+    }
+    let content = "This example will be ready later.";
+    let currentNode: any = expH2[0];
+    while (true) {
+        const nextSibling = _.get(currentNode, 'nextSibling');
+        if (!nextSibling || _.get(nextSibling, 'name') === 'h2') {
+            break;
+        }
 
-// function extractExamples(argNodes: SnippetNodes, $: cheerio.Root): Variable[] {
-//     if (argNodes.snippetNodes.length == 0) return [];
+        currentNode = _.get(currentNode, 'nextSibling');
 
-//     let nodes = argNodes.snippetNodes;
+        // exsit multiple example
+        if (currentNode.type === 'tag' && currentNode.name === 'h3') {
+            currentNode = _.get(currentNode, 'nextSibling');
+            continue;
+        }
 
-//     // Find the first ul
-//     var firstUl = _.find(nodes, (o: any) => o.name == "ul");
-//     if (!firstUl) {
-//         console.error(`Didn't find a UL when searching through snippets on ${extractResourceName($)}`);
-//     }
-// }
+        // only extract the first one
+        if (currentNode.type === 'tag' && currentNode.name === 'pre') {
+            content = $(currentNode).text();
+            break;
+        }
+    }
 
-function extractArguments(argNodes: ArgumentNodes, $: cheerio.Root): Variable[] {
+    return content;
+}
+
+function extractExamples(expNodes: SnippetNodes, $: cheerio.Root): string {
+    let nodes = expNodes.snippetNodes;
+
+    // const snippetText = nodes.map((nn) => $(nn).text()).join('');
+    const textArray = _.map(nodes, node => {
+        return $(node).text();
+    });
+
+    console.debug("[DEBUG]#### len:[%d], textArray:[%v]", textArray.length, textArray);
+
+    const snippetTexts = textArray.join("");
+    console.debug("[DEBUG]#### snippetTexts:[%s]", snippetTexts);
+
+    return snippetTexts;
+}
+
+function extractArguments(argNodes: ArgumentNodes, $: cheerio.Root): variable[] {
     let nodes = argNodes.argumentNodes;
 
     // Find the first ul
-    var firstUl = _.find(nodes, (o: any) => o.name === "ul");
+    let firstUl = _.find(nodes, (o: any) => o.name === "ul");
 
     if (!firstUl) {
         // throw "Didn't find a UL when searching through arguments";
@@ -165,7 +199,7 @@ function extractArguments(argNodes: ArgumentNodes, $: cheerio.Root): Variable[] 
         let text = $(li).text();
         let regex = /([a-zA-Z0-9_]+) (.+)/;
         let result = text.match(regex);
-        var name, description;
+        let name, description;
         if (!result) {
             name = text;
             //console.error(`Didn't find a description for ${text} on ${extractResourceName($)}`);
@@ -178,14 +212,14 @@ function extractArguments(argNodes: ArgumentNodes, $: cheerio.Root): Variable[] 
     });
 }
 
-function extractAttributes(argNodes: AttributeNodes, $: cheerio.Root): Variable[] {
+function extractAttributes(argNodes: AttributeNodes, $: cheerio.Root): variable[] {
     if (argNodes.attributeNodes.length === 0) {
         return [];
     }
 
     let nodes = argNodes.attributeNodes;
     // Find the first ul
-    var firstUl = _.find(nodes, (o: any) => o.name === "ul");
+    let firstUl = _.find(nodes, (o: any) => o.name === "ul");
     if (!firstUl) {
         // console.error(`Didn't find a UL when searching through attributes on ${extractResourceName($)}`);
         return [];
@@ -194,7 +228,7 @@ function extractAttributes(argNodes: AttributeNodes, $: cheerio.Root): Variable[
         let text = $(li).text();
         let regex = /([a-zA-Z0-9_]+) (.+)/;
         let result = text.match(regex);
-        var name, description;
+        let name, description;
         if (!result) {
             name = text;
             // console.error(`Didn't find a description for ${text} on ${extractResourceName($)}`);
@@ -272,23 +306,52 @@ function extractResourceUrl(html: string): Map<string, string> {
 const files = fs.readdirSync(resourcesPath);
 const indexHtml = fs.readFileSync(indexPath, "utf-8");
 Promise.all(getAllParsed(files)).then($s => {
-    const resIndexMap = extractResourceUrl(indexHtml);
-    const resources: Resource[] = _.map($s, $ => {
-        const resName = extractResourceName($);
-        return {
-            name: resName,
-            args: extractArguments(extractArgumentsContent($), $),
-            attrs: extractAttributes(extractAttributesContent($), $),
-            url: resIndexMap.get(resName)
-        };
-    });
-    let transformed = _.transform(resources, (result, value, key) => {
-        result[value.name] = {
-            args: value.args,
-            attrs: value.attrs,
-            url: value.url
-        };
-    }, {});
+    const args = process.argv.slice(2);
+    let type = "";
+    if (args.length > 0) {
+        type = args[0];
+    }
+
+    let transformed: any;
+    if (type === "example") {
+        // example collection
+        const examples: Snippet[] = _.map($s, $ => {
+            const resName = extractResourceName($);
+            return {
+                name: resName,
+                example: extractExampleContent($),
+            };
+        });
+        transformed = _.transform(examples, (result, value) => {
+            result[value.name] = {
+                example: value.example
+            };
+        }, {});
+
+    } else {
+    // resource collection
+        const resIndexMap = extractResourceUrl(indexHtml);
+        const resources: Resource[] = _.map($s, $ => {
+            const resName = extractResourceName($);
+            return {
+                name: resName,
+                args: extractArguments(extractArgumentsContent($), $),
+                attrs: extractAttributes(extractAttributesContent($), $),
+                url: resIndexMap.get(resName)
+            };
+        });
+        transformed = _.transform(resources, (result, value, key) => {
+            result[value.name] = {
+                args: value.args,
+                attrs: value.attrs,
+                url: value.url
+            };
+        }, {});
+    }
 
     console.log(JSON.stringify(transformed));
 });
+
+function fun(v: any, i: any, array: any): (value: string, index: number, array: string[]) => void {
+    throw new Error("Function not implemented.");
+}
